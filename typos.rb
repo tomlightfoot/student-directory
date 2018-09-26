21e9ff673374ceaa552d39d14fa62c4cb7ee0c6e
tomlightfoot <tomlightfoot96@gmail.com>
2018-09-26 13:43:16 +0100

    Finishing all exercises for week4 on typos.rb

--- typos.rb
+++ typos.rb
@@ -1,20 +1,56 @@
+# create an empty array
+@students = []
+
+require 'CSV'
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
+  puts "9. Exit"
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(STDIN.gets.chomp)
+  end
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+      puts "Students have been inputted"
+    when "2"
+      show_students
+    when "3"
+      save_students
+      puts "Students saved"
+    when "4"
+      load_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean, try again"
+  end
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-  @students = []
-  # get the first name
-  name = gets.chomp
-  # while the name is not empty, repeat this code
-  while !name.empty? do
-  # add the student hash to the array
-  @students << {name: name, cohort: :november}
-  puts "Now we have #{@students.count} students"
-  # get another name from the user
-  name = gets.chomp
+  @name = STDIN.gets.chomp
+  while !@name.empty? do
+    add_students
+    puts "Now we have #{@students.count} students"
+    @name = STDIN.gets.chomp
   end
-  # return the array of students
   @students
+end
+
+def add_students
+  @students << {name: @name, cohort: :november}
 end
 
 def show_students
@@ -38,57 +74,34 @@ def print_footer
   puts "Overall, we have #{@students.count} great students"
 end
 
-def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
-  puts "9. Exit"
-end
-
-def process(selection)
-  case selection
-    when "1"
-      input_students
-    when "2"
-      show_students
-    when "3"
-      save_students
-    when "4"
-      load_students
-    when "9"
-      exit
-    else
-      puts "I don't know what you mean, try again"
-  end
-end
-
-def interactive_menu
-  loop do
-    print_menu
-    process(gets.chomp)
-  end
-end
+# DO SACE STUDENTS CSV BIT
 
 def save_students
-  # open the file for writing
-  file = File.open("students.csv", "w")
-  # iterate over the array of students
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
+  CSV.open(@filename, "a") do |file|
+    @students.each do |student|
+      file << [student[:name], student[:cohort]]
+    end
   end
-  file.close
 end
 
 def load_students
-  file = File.open("students.csv", "r")
-  file.readlines.each do |line|
-  name, cohort = line.chomp.split(',')
-    @students << {name: name, cohort: cohort.to_sym}
+  CSV.foreach(@filename) do |line|
+    @name, cohort = line
+    add_students
   end
-  file.close
 end
 
+def try_load_students
+  @filename = ARGV.first
+  @filename = "students.csv" if @filename.nil?
+  if File.exists?(@filename)
+    load_students
+    puts "Loaded #{@students.count} students from #{@filename}"
+  else
+    puts "Sorry, #{@filename} doesn't exist"
+    exit
+  end
+end
+
+try_load_students
 interactive_menu
